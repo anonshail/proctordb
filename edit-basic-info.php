f6d973da8b5fe3de816f5f30ca7ba87865fb22e3
anonshail <32844638+anonshail@users.noreply.github.com>
2018-06-05 21:40:56 +0530

    Add files via upload

--- edit-basic-info.php
+++ edit-basic-info.php
@@ -13,17 +13,53 @@
       <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
     </head>
 
-    <body class="teal accent-3" style="background-image: linear-gradient(teal,#1de9b6,#1de9b6,teal) repeat-background : no-repeat">
+    <body class="teal accent-3" style="background-image: linear-gradient(teal,#1de9b6,#1de9b6,teal)">
 
       <!--JavaScript at end of body for optimized loading-->
       <script type="text/javascript" src="js/materialize.min.js"></script>
 
       <!-- NAVBAR WILL GO HERE-->
 
-      <!--CONTAINER-->
+    <nav>
+        <div class="nav-wrapper container" style="margin-top: 2%">
+       
+          <a href="#" class="brand-logo left"><img src="images/logo.png" class="logo" alt="logo"></a>
+          <a href="#" data-target="mobile-demo" class="sidenav-trigger"><i class="material-icons">menu</i></a>
+          <ul class="right hide-on-med-and-down">
+            <li><a href="#"><i class="material-icons" style="font-size: 320%">home</i></a></li>
+            <li><a href="#"><i class="material-icons" style="font-size: 320%">edit</i></a></li>
+            <li><a href="#"><i class="material-icons" style="font-size: 320%">message</i></a></li>
+            <li><a href="#"><i class="material-icons" style="font-size: 320%;">account_circle</i></a></li>
+          </ul>
+        </div>
+      </nav>
 
+    <ul class="sidenav" id="mobile-demo">
+      <li>
+        <div class="user-view center z-depth-3">
+          <div class="background">
+            <img src="images/side_background.jpeg">
+          </div>
+          <a href="#user"><img class="circle"><i class="material-icons" style="font-size: 500%">account_circle</i></a>
+          <a href="#name"><span class="white-text name">name</span></a>
+          <a href="#email"><span class="white-text email">name@gmail.com</span></a>
+        </div>
+      </li>
+      <li class="icons"><a href="#"><i class="material-icons left teal-text">home</i>Home</a></li>
+      <li class="icons"><a href="#"><i class="material-icons left teal-text">edit</i>Edit</a></li>
+      <li class="icons"><a href="#"><i class="material-icons left teal-text">message</i>Messages</a></li>
+    </ul>
+
+    <!-- End of nav bar -->
+    <script type="text/javascript" src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
+    <script type="text/javascript" src="js/materialize.min.js"></script>
+    <script type="text/javascript" src="js/init.js"></script>
+
+      <!--CONTAINER-->
+      
       <div class="container">
-        <h1 class="flow-text center-align">Edit Basic Information</h1>
+      <div class="card-panel">
+        <h1 class="center-align">Basic Information</h1>
 
         <br>
         <h2 class="flow-text">Your Basic Information</h2>
@@ -32,36 +68,36 @@
           <div class="row">
             <div class="col s6 input-field">  <!--NAME-->
               <input type="text" id="name"></input>
-              <label class="active" for="name">Full Name</label>
+              <label class="active black-text" for="name">Full Name</label>
             </div>
 
             <div class="col s6 input-field">  <!--USN-->
               <input type="text" id="usn"></input>
-              <label class="active" for="usn">USN</label>
+              <label class="active black-text" for="usn">USN</label>
             </div>
           </div>
 
           <div class="row">
             <div class="col s6 input-field">  <!--DOB-->
               <input type="date" id="dob" calss="validate"></input>
-              <label class="active" for="dob">Date Of Birth</label>
+              <label class="active black-text" for="dob">Date Of Birth</label>
             </div>
 
             <div class="col s6 input-field">  <!--Phone Number-->
               <input id="icon_telephone" type="tel" class="validate">
-              <label for="icon_telephone">Mobile Number</label>
+              <label class="active black-text" for="icon_telephone">Mobile Number</label>
             </div>
           </div>
 
            <div class="row">
             <div class="col s6 input-field">  <!--Blood Group-->
               <input type="text" id="bloodg"></input>
-              <label class="active" for="bloodg">Blood Group</label>
+              <label class="active black-text" for="bloodg">Blood Group</label>
             </div>
 
             <div class="col s6 input-field">  <!--Email ID-->
               <input id="Email" type="Email" class="validate">
-              <label for="Email">Email</label>
+              <label for="Email" class="active black-text">Email</label>
             </div>
           </div>
 
@@ -71,24 +107,24 @@
           <div class="row">
             <div class="col s6 input-field">  <!--Father's Name-->
               <input type="text" id="fname"></input>
-              <label class="active" for="fname">Father's Name</label>
+              <label class="active black-text" for="fname">Father's Name</label>
             </div>
 
             <div class="col s6 input-field">  <!--father's Email ID-->
               <input id="femail" type="Email" class="validate">
-              <label for="femail">Father's Email</label>
+              <label class="active black-text" for="femail">Father's Email</label>
             </div>
           </div>
 
           <div class="row">
             <div class="col s6 input-field">  <!--Father's occupation-->
               <input type="text" id="foccupation"></input>
-              <label class="active" for="foccupation">Occupation</label>
+              <label class="active black-text" for="foccupation">Occupation</label>
             </div>
 
             <div class="col s6 input-field">  <!--father's conatct-->
               <input id="ftel" type="tel" class="validate">
-              <label for="ftel">Father's Contact Number</label>
+              <label class="active black-text" for="ftel">Father's Contact Number</label>
             </div>
           </div>
 
@@ -98,24 +134,24 @@
           <div class="row">
             <div class="col s6 input-field">  <!--Mother's Name-->
               <input type="text" id="mname"></input>
-              <label class="active" for="mname">Mother's Name</label>
+              <label class="active black-text" for="mname">Mother's Name</label>
             </div>
 
             <div class="col s6 input-field">  <!--Mother's Email ID-->
               <input id="memail" type="Email" class="validate">
-              <label for="memail">Mother's Email</label>
+              <label class="active black-text" for="memail">Mother's Email</label>
             </div>
           </div>
 
           <div class="row">
             <div class="col s6 input-field">  <!--Mother's occupation-->
               <input type="text" id="moccupation"></input>
-              <label class="active" for="moccupation">Occupation</label>
+              <label class="active black-text" for="moccupation">Occupation</label>
             </div>
 
             <div class="col s6 input-field">  <!--Mother's conatct-->
               <input id="mtel" type="tel" class="validate">
-              <label for="mtel">Mother's Contact Number</label>
+              <label class="active black-text" for="mtel">Mother's Contact Number</label>
             </div>
           </div>
 
@@ -125,24 +161,24 @@
           <div class="row">
             <div class="col s6 input-field">  <!--LG's Name-->
               <input type="text" id="lgname"></input>
-              <label class="active" for="lgname">Gaurdian's Name</label>
+              <label class="active black-text" for="lgname">Gaurdian's Name</label>
             </div>
 
             <div class="col s6 input-field">  <!--LG's Email ID-->
               <input id="lgemail" type="Email" class="validate">
-              <label for="lgemail">Gaurdian's Email</label>
+              <label class="active black-text" for="lgemail">Gaurdian's Email</label>
             </div>
           </div>
 
           <div class="row">
             <div class="col s6 input-field">  <!--LG's occupation-->
               <input type="text" id="lgoccupation"></input>
-              <label class="active" for="lgoccupation">Occupation</label>
+              <label class="active black-text" for="lgoccupation">Occupation</label>
             </div>
 
             <div class="col s6 input-field">  <!--LG's conatct-->
               <input id="lgtel" type="tel" class="validate">
-              <label for="lgtel">Gaurdian's Contact Number</label>
+              <label class="active black-text" for="lgtel">Gaurdian's Contact Number</label>
             </div>
           </div>
 
@@ -152,17 +188,17 @@
           <div class="row">
             <div class="col s4 input-field">  
               <input type="text" id="xboard"></input>
-              <label class="active" for="xboard">Board</label>
+              <label class="active black-text" for="xboard">Board</label>
             </div>
 
             <div class="col s4 input-field">  
               <input id="xyear" type="text" class="validate">
-              <label for="xyear">Year of Passing</label>
+              <label class="active black-text" for="xyear">Year of Passing</label>
             </div>
 
             <div class="col s4 input-field">  
               <input id="xperc" type="text">
-              <label for="xperc">Percentage</label>
+              <label class="active black-text" for="xperc">Percentage</label>
             </div>
           </div>
 
@@ -171,26 +207,25 @@
           <div class="row">
             <div class="col s4 input-field">  
               <input type="text" id="xiiboard"></input>
-              <label class="active" for="xiiboard">Board</label>
+              <label class="active black-text" for="xiiboard">Board</label>
             </div>
 
             <div class="col s4 input-field"> 
               <input id="xiiyear" type="text" class="validate">
-              <label for="xiiyear">Year of Passing</label>
+              <label class="active black-text" for="xiiyear">Year of Passing</label>
             </div>
 
             <div class="col s4 input-field">
               <input id="xiiperc" type="text">
-              <label for="xiiperc">Percentage</label>
+              <label class="active black-text" for="xiiperc">Percentage</label>
             </div>
           </div>
 
           <br>
-          <div class="center-align"><a class="waves-effect waves-light btn"><i class="material-icons right">send</i>Submit</a></div>
+          <div class="center-align"><a class="waves-effect waves-light btn red ligten-3"><i class="material-icons right">send</i>Submit</a></div>
           <br><br>
         </form> <!--END OF FORM-->        
-
       </div>
-
+      </div>
     </body>
   </html>
